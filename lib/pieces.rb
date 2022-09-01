999094971657878932590f2b3f433401a75da885
Jozef Hudec <jojohudec27@gmail.com>
2022-09-01 19:47:48 +0200

    Add #unicode method to piece classes

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,3 +1,5 @@
+require_relative 'display'
+
 class Piece
   def initialize(color)
     @color = color
@@ -5,19 +7,55 @@ class Piece
 end
 
 class King < Piece
+  def unicode
+    @color == 'black' ? Display.king_black : Display.king_white
+  end
+
+  def self.available_moves(row, col, board)
+  end
 end
 
 class Queen < Piece
+  def unicode
+    @color == 'black' ? Display.queen_black : Display.queen_white
+  end
+
+  def self.available_moves(row, col, board)
+  end
 end
 
 class Rook < Piece
+  def unicode
+    @color == 'black' ? Display.bishop_black : Display.bishop_white
+  end
+
+  def self.available_moves(row, col, board)
+  end
 end
 
 class Bishop < Piece
+  def unicode
+    @color == 'black' ? Display.bishop_black : Display.bishop_white
+  end
+
+  def self.available_moves(row, col, board)
+  end
 end
 
 class Knight < Piece
+  def unicode
+    @color == 'black' ? Display.knight_black : Display.knight_white
+  end
+
+  def self.available_moves(row, col, board)
+  end
 end
 
 class Pawn < Piece
+  def unicode
+    @color == 'black' ? Display.pawn_black : Display.pawn_white
+  end
+
+  def self.available_moves(row, col, board)
+  end
 end
